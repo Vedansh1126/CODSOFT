ae4d6d6858c640ab466c89dffb793351b876723d
Vedansh1126 <146796194+Vedansh1126@users.noreply.github.com>
2025-08-06 11:41:38 +0530

    Update ATMInterface.java

--- src/ATMInterface.java
+++ src/ATMInterface.java
@@ -85,7 +85,7 @@ class ATM {
 
 public class ATMInterface {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount(1000); // Initial balance
+        BankAccount account = new BankAccount(1000);
         ATM atm = new ATM(account);
         atm.start();
     }
